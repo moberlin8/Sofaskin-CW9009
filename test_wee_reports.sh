c56ce2e334fd1a9932f22853183399d9a3314695
Matt Oberlin <moberlin8@gmail.com>
2018-01-05 12:00:17 -0800

    Test Python script (wee_reports) for errors

---
+++ test_wee_reports.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# mao need to be able to test if the python script (wee_reports) worked
+
+wee_reports 2> errors.txt
+
+if grep -q -i err errors.txt
+then
+  echo "Founds errors"
+  exit 1
+elif grep -q -i traceback errors.txt
+then
+  echo "Found traceback"
+  exit 2
+elif [ ! -s errors.txt ]
+then
+  echo "Looks clean"
+  exit 0
+else
+  echo "Should be clean"
+  exit 0
+fi
+
+cat errors.txt
+
