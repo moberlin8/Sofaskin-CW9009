19656ff36380d11033ce4059c537fe6073e01806
Matt Oberlin <moberlin8@gmail.com>
2018-01-06 10:50:35 -0800

    Restart weewx service after change config

--- setup_reports.sh
+++ setup_reports.sh
@@ -6,6 +6,5 @@ mkdir -p /tmp/weewx/html
 mkdir -p /tmp/weewx/skins/oberlintv
 cp -rv . /tmp/weewx/skins/oberlintv
 cp -v ./weewx-test.conf  /etc/weewx/weewx.conf
-
-# mao 2018-Jan-6 Give time to generate a few archive records
-sleep 39
+# mao 2018-Jan-6 Make sure we pickup new config
+sudo service weewx restart
