ca537ef1ba601059a83e7ee2ffd8ec6fdd6fed3b
Matt Oberlin <moberlin8@gmail.com>
2018-01-06 08:46:13 -0800

    Touch mod time to fix time issue in wee_reports

--- test_wee_reports.sh
+++ test_wee_reports.sh
@@ -6,8 +6,8 @@ set -x
 
 cp --verb ./bin/user/historygenerator.py /usr/share/weewx/user/
 
-# mao 2018-Jan-6 Hack for "time" issue
-sleep 120
+# mao 2018-Jan-6 another hack to fix "time" issue, set mod time backwards 10 minutes
+touch -B 600 /tmp/weewx/skins/oberlintv/*
 
 wee_reports --config=weewx-test.conf 2> errors.txt 1> output.txt
 
